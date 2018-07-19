526932baa2a35f6021cd1547708f93ff0b0e0e9e
vagrant <vagrant@homestead>
2018-07-19 11:45:11 +0000

    auth e lang

---
+++ resources/lang/pt-br/register.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Register'         => 'Registre-se',
+    'Name'             => 'Nome',
+    'Email'            => 'Email',
+    'Password'         => 'Senha',
+    'Confirm'          => 'Confirme a Senha',
+];
